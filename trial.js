59264b9f06f90a53daaddb20970701cf0d1696bd
harbike <adenlekehny@gmail.com>
2024-10-31 12:03:59 +0100

    fixed error handling for inputs

--- trial.js
+++ trial.js
@@ -1,4 +1,5 @@
 "use strict";
+// NOTE TO SELF::: Some functions malfunction when variable declarations are made outside of them.
 const calC = document.querySelector("html");
 
 // params to clear
@@ -9,6 +10,7 @@ const resultDisplayEmpty = document.querySelector(".result__display-empty");
 const resultDisplayCompleted = document.querySelector(
   ".result__display-completed"
 );
+const errorMessages = document.querySelectorAll(".input__field span");
 
 // FUNCTION - clear computations
 function clearComputations() {
@@ -16,8 +18,21 @@ function clearComputations() {
   calcInputs.forEach((calcInput) => {
     calcInput.value = "";
     console.log(`i was clicked`);
+
     radioButtons.forEach((radioBtn) => {
       radioBtn.checked = false;
+    });
+
+    // code to clear error message and errorStyles
+    const inputFieldTexts = document.querySelectorAll(".input__field-text");
+    inputFieldTexts.forEach((text, index) => {
+      const icon = inputFieldIcons[index];
+      const error = errorMessages[index];
+
+      text.classList.remove("text-error");
+      icon.classList.remove("icon-error");
+      error.classList.remove("span");
+      error.classList.add("no_display");
     });
   });
 
@@ -30,12 +45,6 @@ function clearComputations() {
 clearBtn.onclick = () => {
   clearComputations();
 };
-
-// params to compute
-const mortgageAmount = document.querySelector("#text__mort-amount").value;
-const mortgageTerm = document.querySelector("#text__mort-term").value;
-const interestRate = document.querySelector("#text__mort-rate").value;
-const mortgageType = document.querySelector(".radio-btn:checked");
 
 // FUNCTION - calculate mortgage
 function calculateMortgage() {
@@ -88,83 +97,99 @@ calcBtn.onclick = () => {
 // params to handle error
 const inputFieldRadios = document.querySelectorAll(".input__field-radio");
 
-// const mortAmountText = document.querySelector("#text__mort-amount");
-// const mortAmountIcon = document.querySelector("#icon__mort-amount");
-// const mortAmountErr = document.querySelector(".amount_err");
-
-// const mortTermText = document.querySelector("#text__mort-term");
-// const mortTermIcon = document.querySelector("#icon__mort-term");
-// const mortTermErr = document.querySelector(".term_err");
-
-// const mortRateText = document.querySelector("#text__mort-rate");
-// const mortRateIcon = document.querySelector("#icon__mort-rate");
-// const mortRateErr = document.querySelector(".rate_err");
-
-// const mortTypeErr = document.querySelector(".type_err");
-
 // FUNCTION - confirm all inputs are filled
 function checkInputs() {
   const mortgageAmount = document.querySelector("#text__mort-amount").value;
   const mortgageTerm = document.querySelector("#text__mort-term").value;
   const interestRate = document.querySelector("#text__mort-rate").value;
   const mortgageType = document.querySelector(".radio-btn:checked");
-  
+
   const mortAmountText = document.querySelector("#text__mort-amount");
-const mortAmountIcon = document.querySelector("#icon__mort-amount");
-const mortAmountErr = document.querySelector(".amount_err");
+  const mortAmountIcon = document.querySelector("#icon__mort-amount");
+  const mortAmountErr = document.querySelector(".amount_err");
 
-const mortTermText = document.querySelector("#text__mort-term");
-const mortTermIcon = document.querySelector("#icon__mort-term");
-const mortTermErr = document.querySelector(".term_err");
+  const mortTermText = document.querySelector("#text__mort-term");
+  const mortTermIcon = document.querySelector("#icon__mort-term");
+  const mortTermErr = document.querySelector(".term_err");
 
-const mortRateText = document.querySelector("#text__mort-rate");
-const mortRateIcon = document.querySelector("#icon__mort-rate");
-const mortRateErr = document.querySelector(".rate_err");
+  const mortRateText = document.querySelector("#text__mort-rate");
+  const mortRateIcon = document.querySelector("#icon__mort-rate");
+  const mortRateErr = document.querySelector(".rate_err");
 
-const mortTypeErr = document.querySelector(".type_err");
-
-
+  const mortTypeErr = document.querySelector(".type_err");
 
   // check all inputs
   if (
-    (!mortgageAmount ||
-    isNaN(mortgageAmount)) ||
-    (!mortgageTerm ||
-    isNaN(mortgageTerm)) ||
-    (!interestRate ||
-    isNaN(interestRate)) ||
-    (!mortgageType)
+    !mortgageAmount ||
+    isNaN(mortgageAmount) ||
+    !mortgageTerm ||
+    isNaN(mortgageTerm) ||
+    !interestRate ||
+    isNaN(interestRate) ||
+    !mortgageType
   ) {
     alert(`fill all fields`);
 
     // check for each inputs
     // mortgageAmount
-    if (!mortgageAmount ||
-        isNaN(mortgageAmount)) {
-    console.log(`helpaaaaa`);
-    mortAmountErr.classList.remove('no_display');
-    mortAmountErr.classList.add('span');
-    mortAmountText.classList.add('text-error');
-    mortAmountIcon.classList.add('icon-error');
+    if (!mortgageAmount || isNaN(mortgageAmount)) {
+      console.log(`helpaaaaa`);
+      mortAmountErr.classList.remove("no_display");
+      mortAmountErr.classList.add("span");
+      mortAmountText.classList.add("text-error");
+      mortAmountIcon.classList.add("icon-error");
 
-     // to remove prompt message when error is rectified
-     mortAmountText.addEventListener('input', () => {
-        if(mortAmountErr.classList.contains('span')){
-            mortAmountErr.classList.add('no_display');
-            mortAmountErr.classList.remove('span');
-            mortAmountText.classList.remove('text-error');
-            mortAmountIcon.classList.remove('icon-error');
+      // to remove prompt message when error is rectified
+      mortAmountText.addEventListener("input", () => {
+        if (mortAmountErr.classList.contains("span")) {
+          mortAmountErr.classList.add("no_display");
+          mortAmountErr.classList.remove("span");
+          mortAmountText.classList.remove("text-error");
+          mortAmountIcon.classList.remove("icon-error");
         }
-    })
+      });
     }
-   
 
+    // mortgageTerm
+    if (!mortgageTerm || isNaN(mortgageTerm)) {
+      console.log(`yo-yo!`);
+      mortTermErr.classList.remove("no_display");
+      mortTermErr.classList.add("span");
+      mortTermText.classList.add("text-error");
+      mortTermIcon.classList.add("icon-error");
 
+      // to remove prompt message when error is rectified
+      mortTermText.addEventListener("input", () => {
+        if (mortTermErr.classList.contains("span")) {
+          mortTermErr.classList.add("no_display");
+          mortTermErr.classList.remove("span");
+          mortTermText.classList.remove("text-error");
+          mortTermIcon.classList.remove("icon-error");
+        }
+      });
+    }
 
+    // interestRate
+    if (!interestRate || isNaN(interestRate)) {
+      console.log(`yo-yo!`);
+      mortRateErr.classList.remove("no_display");
+      mortRateErr.classList.add("span");
+      mortRateText.classList.add("text-error");
+      mortRateIcon.classList.add("icon-error");
+
+      // to remove prompt message when error is rectified
+      mortRateText.addEventListener("input", () => {
+        if (mortRateErr.classList.contains("span")) {
+          mortRateErr.classList.add("no_display");
+          mortRateErr.classList.remove("span");
+          mortRateText.classList.remove("text-error");
+          mortRateIcon.classList.remove("icon-error");
+        }
+      });
+    }
   }
 }
 
-// **********************
 // params for states
 const inputFieldTexts = document.querySelectorAll(".input__field-text");
 const inputFieldIcons = document.querySelectorAll(".input__field-icon");
@@ -194,3 +219,14 @@ inputFieldRadios.forEach((x) => {
     x.classList.remove("field-active");
   });
 });
+
+// FUNCTION - hide result
+function hideResult() {
+  resultDisplayEmpty.classList.remove("no_display");
+  resultDisplayCompleted.classList.add("no_display");
+}
+// FUNCTION - show result
+function showResult() {
+  resultDisplayEmpty.classList.add("no_display");
+  resultDisplayCompleted.classList.remove("no_display");
+}
